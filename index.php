ebe15635faae2bc00f64984067f2f8770dc68c8a
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-02-25 16:20:07 +0100

    print and css

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
-$key = "e0151c8f32093eec292358373e03c7c1";
-$json = file_get_contents("https://api.themoviedb.org/3/movie/upcoming?api_key=$key&language=it");
+$key = 'e0151c8f32093eec292358373e03c7c1';
+$json = file_get_contents(
+    "https://api.themoviedb.org/3/movie/upcoming?api_key=$key&language=it"
+);
 
 $response = json_decode($json, true);
 $result = $response['results'];
 // var_dump($result);
 
-
-class UpcomingMovie {
+class UpcomingMovie
+{
     public $title;
     public $overview;
     public $photo_url;
@@ -17,21 +19,60 @@ class UpcomingMovie {
 
     function __construct($_result)
     {
-        $this->title=$_result['original_title'];
-        $this->overview=$_result['overview'];
-        $this->photo_url="https://www.themoviedb.org/t/p/original".$_result['poster_path'];
-        $this->genres=$_result['genre_ids'];
-        $this->release_date=$_result['release_date'];
+        $this->title = $_result['original_title'];
+        $this->overview = $_result['overview'];
+        $this->photo_url =
+            'https://www.themoviedb.org/t/p/original' . $_result['poster_path'];
+        $this->genres = $_result['genre_ids'];
+        $this->release_date = date("d-m-Y", strtotime($_result['release_date']));
+    }
+
+    public function printMovie()
+    {
+        echo "<div class='card'>";
+        echo "<h1>$this->title</h1>";
+        echo "<div class='poster'>
+                <p>$this->overview</p>
+                <img src=\"$this->photo_url\">
+            </div>";
+        if($this->release_date>date("d-m-Y")){
+            echo "<p>In uscita il $this->release_date</p>";
+        }else{
+            echo "<p>Uscito il $this->release_date</p>";
+        }
+        echo '</div>';
     }
 }
 
-$movies=[];
-foreach($result as $key => $value){
+$movies = [];
+foreach ($result as $key => $value) {
     array_push($movies, "movie_$key");
 }
+
 // var_dump($movies);
-foreach($movies as $key => $value){
-    $value= new UpcomingMovie($result[$key]);
-    var_dump($value);
-}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upcoming Movies</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>MovieDBool</h1>
+    </header>
+    <main>
+        <?php foreach ($movies as $key => $value) {
+            $value = new UpcomingMovie($result[$key]);
+            $value->printMovie();
+            // var_dump($value);
+        } ?>
+    </main>
+    <footer>
+        <h1>MovieDBool</h1>
+    </footer>
+</body>
+</html>
